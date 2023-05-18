1bf3656dae2f85df6cf2196104f73e40d0157352
yasmitch <yasminem@stanford.edu>
2023-05-17 22:52:40 -0700

    Corrected tests

--- calculator/tests/your_tests.py
+++ calculator/tests/your_tests.py
@@ -4,10 +4,10 @@ from calculator_adapter import run
 
 ### ADD AT LEAST TWO TESTS HERE!
 
-assert run("2*6").output == "12"
-assert run("10/5").output == "2"
-assert run("12+7").output == "19"
-assert run("21-5").output == "16"
+assert run("2 * 6").output == "12"
+assert run("10 / 5").output == "2"
+assert run("12 + 7").output == "19"
+assert run("21 - 5").output == "16"
 
 ###
 
